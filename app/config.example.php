3f31ee69c67c3ee7a87d4af9a3fe2ccc75eaa3ab
mitchellurgero <info@urgero.org>
2018-03-31 20:27:46 +0000

    First version readme

--- app/config.example.php
+++ app/config.example.php
@@ -3,14 +3,14 @@
 $config = array(
 	"siteName"			=> "TinySocial!",									// Default site name
 	"siteDesc"			=> "Flat File social network!",						// Default site description
-	"sitePath"			=> "http://192.168.1.12/codiad/workspace/tinysocial/", //where is the site located (host and path!) - use a trailing slash!
-	"databaseLocation"	=> "/var/www/html/codiad/workspace/databases",		// database location
+	"sitePath"			=> "http://192.168.1.12/codiad/workspace/tinysocial/", // where is the site located (host and path!) - use a trailing slash!
+	"databaseLocation"	=> "/var/www/html/codiad/workspace/databases",		// database location (Folder to hold the database)
 	"databaseName"		=> "tinysocial",									// database name
-	"timezone"			=> "America/Chicago",								//set site default timezone
-	"registration"		=> true,											//Allow registration
+	"timezone"			=> "America/Chicago",								// set site default timezone (Important for timestamping!)
+	"registration"		=> true,											// Allow registration
 	
 	
-	"debug"				=> true,
+	"debug"				=> true,											// Enable debug (All error and Warning output)
 	);
 	
-	addPlugin("example");
+	//addPlugin("example"); //Plugin support not working ATM.
